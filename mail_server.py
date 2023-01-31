5b06ba81e013f80d9e47063536b8e13da6bcccc0
mhmdhuss44 <102224426+mhmdhuss44@users.noreply.github.com>
2023-01-31 17:46:16 +0200

    added some changes/comments

--- mail_server.py
+++ mail_server.py
@@ -4,13 +4,14 @@ from email.mime.text import MIMEText
 import smtplib
 import yaml
 
+
 class MailServer:
 
     def __init__(self):
         self.CONFIGS = None
 
     def load_mail_server(self):
-        conf_yaml=paths.yaml_path
+        conf_yaml = paths.yaml_path
         if conf_yaml.exists():
             with open(file=conf_yaml, mode="r") as yaml_file:
                 try:
@@ -19,31 +20,68 @@ class MailServer:
                 except yaml.YAMLError as ex:
                     print(f"Failed to load Mail Server configuration file")
 
-    def send_email(self,email_from:str,email_to:str,email_subject:str,email_message:str) -> bool:
-    
+    #  it will open the file and read the content using the yaml.safe_load() method
+    #    , and store it in the CONFIGS attribute of the class.
+
+
+
+    # This function is a method of the MailServer class, it sends an email using the Simple Mail
+    # Transfer Protocol (SMTP) library in python.
+    def send_email(self, email_from: str, email_to: str, email_subject: str, email_message: str) -> bool:
+
         email_Result = False
         # initialize connection to our email server, we will use Outlook here
+        # this info is located in the yaml file
         smtp = smtplib.SMTP(self.CONFIGS["EMAIL_SMTP"], port=self.CONFIGS["EMAIL_PORT"])
 
+        # what are those exactly?
         smtp.ehlo()  # send the extended hello to our server
         smtp.starttls()  # tell server we want to communicate with TLS encryption
 
-        smtp.login(self.CONFIGS["EMAIL_USER"],self.CONFIGS["EMAIL_PASS"] )  # login to our email server
+        smtp.login(self.CONFIGS["EMAIL_USER"], self.CONFIGS["EMAIL_PASS"])  # login to our email server
 
         # setup the parameters of the message
-        msg = MIMEMultipart() 
+        msg = MIMEMultipart()
         message = email_message
-        msg["from"]=email_from
-        msg["to"]=email_to
-        msg["subject"]=email_subject
+        msg["from"] = email_from
+        msg["to"] = email_to
+        msg["subject"] = email_subject
 
         # add in the message body
         msg.attach(MIMEText(message, 'plain'))
-                    
+
         try:
             smtp.send_message(msg)
             del msg
-            email_Result= True
+            email_Result = True
         except Exception as mail_exception:
             print(f"Email was not sent with the following error:[{mail_exception}]")
         return email_Result
+
+
+
+#     The first step is to create a connection to the email server using the SMTP class from the smtplib
+#     library. The server's address and port are passed to the constructor of the SMTP class,
+#     and the connection is established. The server's address and port are obtained from the yaml file.
+#
+# The second step is to initiate a handshake with the server. The ehlo() method is used to
+# send an extended hello command to the server, which starts the handshake process.
+#
+# The third step is to establish a secure connection with the server. The starttls() method
+# is used to tell the server that we want to communicate with it using
+# Transport Layer Security (TLS) encryption. This step is optional but highly recommended.
+#
+# The fourth step is to authenticate with the server. The login() method is used
+# to send the login credentials (username and password) to the server.
+# These credentials are obtained from the yaml file as well
+#
+# The fifth step is to create the email message. A MIMEMultipart object is
+# created and the various fields of the email message, such as the sender, recipient, subject,
+# and message body, are set.
+#
+# The sixth step is to send the email message. The send_message() method is used
+# to send the email message to the server.
+#
+# The final step is to check if the email was sent successfully and return
+# the result. If the email was sent successfully, the function returns True,
+# otherwise it returns False and print the exception error.
--- main.py
+++ main.py
@@ -1,22 +1,24 @@
 from mail_server import MailServer
+from personClass import Person
+
 
 def run():
-    email_from="men_tor_ing_2023@outlook.com"
-    email_to="orsan.awawdi@gmail.com"
+    email_from = "men_tor_ing_2023@outlook.com"
+    # email_to="mhmdhuss44@gmail.com"
     subject = "This is a testing email"
-    message="Hi from my email server"
+    message = "Hi from my email server2"
 
-    my_mail=MailServer()
+    my_mail = MailServer()
     my_mail.load_mail_server()
-    send_email_result = my_mail.send_email(email_from=email_from,email_to=email_to,email_subject=subject,email_message=message)
+    send_email_result = my_mail.send_email(email_from=email_from, email_to=mhmd.get_email_address(),
+                                           email_subject=subject, email_message=message)
 
     if send_email_result:
-        print(f"Email was sent successfuly to [{email_to}]")
+        print(f"Email was sent successfuly to [{mhmd.get_email_address()}]")
     else:
         print("Error while sending email")
 
-if __name__=="__main__":
+
+if __name__ == "__main__":
+    mhmd = Person("mohamed", 21, "0549366894", "mhmdhuss44@gmail.com")
     run()
-
-
-
